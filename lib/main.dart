b5c5e50bb11713fe31c5ea3c7c30eadaaf2905dd
mohamed kohla <102987842+Kohlaa@users.noreply.github.com>
2023-05-31 18:55:46 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+import 'bmi_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        scaffoldBackgroundColor: const Color(0xFF111328),
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color(0xFF111328),
+          elevation: 0.0,
+          iconTheme: IconThemeData(
+            color: Colors.grey,
+          ),
+        ),
+      ),
+      home: const BmiScreen(),
+    );
+  }
+}
